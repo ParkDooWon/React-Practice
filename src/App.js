7c3909a2a87bda7773e5d5a4f682ec7b871f2e0d
ParkDooWon <pass345@naver.com>
2020-06-28 17:53:34 +0900

    feat: 1-4 JSX 사용 - JSX 사용하기 - style : css사용 - 주석 처리

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
 import React from 'react';
 import Hello from './Hello';
+import './App.css';
 
 function App() {
+  const name = 'react';
+  const style = {
+      backgroundColor: 'black',
+      color: 'aqua',
+      fontSize: 64,
+      padding: 93
+  }
+
   return (
-      <div>
-        <Hello />
-      </div>
-  );
+      <>
+          {/*이건 안보임*/}
+          /*이건 보임*/
+          <Hello />
+          <div style={style}>
+              {/*이렇게 주석도 가능*/}
+              {name}</div>
+          <div className="gray-box">dd</div>
+      </>
+  )
 }
 
 export default App;
