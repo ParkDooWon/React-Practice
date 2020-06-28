c1de965cec4435ec47d89d02ecc0936cc1aa269d
ParkDooWon <pass345@naver.com>
2020-06-28 18:20:08 +0900

    feat: 1-5 Props 사용 - Props 사용 : 값을 전달한다. - children 사용 : <Wrapper> 컴포넌트 태그 안에 있는 값들을 조회

--- src/App.js
+++ src/App.js
@@ -1,27 +1,17 @@
 import React from 'react';
 import Hello from './Hello';
+import Wrapper from './Wrapper';
 import './App.css';
 
 function App() {
-  const name = 'react';
-  const style = {
-      backgroundColor: 'black',
-      color: 'aqua',
-      fontSize: 64,
-      padding: 93
-  }
 
   return (
-      <>
-          {/*이건 안보임*/}
-          /*이건 보임*/
-          <Hello />
-          <div style={style}>
-              {/*이렇게 주석도 가능*/}
-              {name}</div>
-          <div className="gray-box">dd</div>
-      </>
-  )
+      <Wrapper>
+        <Hello name = 'react' color='red'/>
+        <Hello color='green'/>
+        <Hello/>
+      </Wrapper>
+  );
 }
 
 export default App;
