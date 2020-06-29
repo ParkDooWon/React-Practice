099756b03fec63808d7a40e349693ff932824d82
ParkDooWon <pass345@naver.com>
2020-06-29 12:08:50 +0900

    feat: 1-9 여러 개의 input 상태 관리하기 - useState에 객체의 상태 관리 - 객체의 상태 수정 (불변성을 지켜야 리렌더링 된다.)

--- src/inputSample.js
+++ src/inputSample.js
@@ -1,23 +1,36 @@
 import React, {useState} from 'react';
 
 function InputSample() {
-    const [text, setText] = useState('');
+    const [inputs, setInputs] = useState({
+        name: '',
+        nickname: ''
+    });
+
+    const {name, nickname} = inputs;
 
     const onChange = e => {
-        setText(e.target.value);
-    }
+        const {value, name} = e.target;
+        setInputs({
+            ...inputs,  // spread 문법. 객체의 내용을 펼쳐서 기존 객체를 복사한다.
+            [name]: value
+        });
+    };
 
     const onReset = () => {
-        setText('');
-        // document.querySelector('input').value = '';   -> 아래처럼 하지않고 input의 value를 직접 조작할 수도 있다.
-    }
+        setInputs({
+            name: '',
+            nickname: ''
+        })
+    };
 
     return (
         <div>
-            <input onChange={onChange} value={text}/>  {/*input의 value도 업데이트 해준다.*/}
+            <input name="name" placeholder="이름" onChange={onChange} value={name}/>
+            <input name="nickname" placeholder="닉네임" onChange={onChange} value={nickname}/>
             <button onClick={onReset}>초기화</button>
             <div>
-                <b>값: {text}</b>
+                <b>값: </b>
+                {name} ({nickname})
             </div>
         </div>
     )
