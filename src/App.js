9272d72a265a1ea35824921d1e0d9ff6aa673619
ParkDooWon <pass345@naver.com>
2020-07-01 18:20:30 +0900

    feat: 1-15 배열 항목 수정하기 - 상태에 따라 나오는 결과를 다르게 하기 (삼항연사자 사용) - useState에서 map을 이용하여 배열을 수정

--- src/App.js
+++ src/App.js
@@ -19,17 +19,20 @@ function App() {
     {
       id: 1,
       username: 'a',
-      email: 'email1'
+      email: 'email1',
+      active: true
     },
     {
       id: 2,
       username: 'b',
-      email: 'email2'
+      email: 'email2',
+      active: true
     },
     {
       id: 3,
       username: 'c',
-      email: 'email3'
+      email: 'email3',
+      active: false
     }
   ]);
 
@@ -51,6 +54,12 @@ function App() {
   const onRemove = id => {
     setUsers(users.filter(user => user.id !== id))
   }
+  const onToggle = id => {  // 완전 탐색으로 모든 users를 확인하여 id를 맞춰본다.
+    setUsers(
+        users.map(user =>   //배열의 불변성을 유지하면서 배열을 업데이트할 때 map을 사용할 수 있다.
+        user.id === id? {...user, active: !user.active} : user)
+    )
+  }
   return (
       <>
         <CreateUser
@@ -59,7 +68,7 @@ function App() {
             onChange={onChange}
             onCreate={onCreate}
         />
-        <UserList users={users} onRemove={onRemove}/>
+        <UserList users={users} onRemove={onRemove} onToggle={onToggle}/>
       </>
   );
 }
