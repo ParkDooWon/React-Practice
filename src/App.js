a2a5ceb3580d30af5cb7c2706d88e5dd0478786f
ParkDooWon <pass345@naver.com>
2020-07-04 16:59:11 +0900

    feat: TodoList를 위한 컴포넌트 UI 만들기

--- src/App.js
+++ src/App.js
@@ -1,119 +1,25 @@
-import React, { useRef, useReducer, useMemo, useCallback } from 'react';
-import UserList from './UserList';
-import CreateUser from './CreateUser';
+import React from 'react';
+import { createGlobalStyle } from 'styled-components';
+import TodoTemplate from './components/TodoTemplate';
+import TodoHead from './components/TodoHead';
+import TodoList from "./components/TodoList";
+import TodoCreate from "./components/TodoCreate";
 
-function countActiveUsers(users) {
-  console.log('count starting!');
-  return users.filter(user => user.active).length;
-}
-
-const initialState = {
-  inputs: {
-    username: '',
-    email: ''
-  },
-  users: [
-    {
-      id: 1,
-      username: 'a',
-      email: 'email1',
-      active: true
-    },
-    {
-      id: 2,
-      username: 'b',
-      email: 'email2',
-      active: true
-    },
-    {
-      id: 3,
-      username: 'c',
-      email: 'email3',
-      active: false
+const GlobalStyle = createGlobalStyle`
+    body {
+      background: #e9ecef
     }
-  ]
-};
-
-function reducer(state, action) {
-  switch (action.type) {
-    case 'CHANGE_INPUT' :
-      return {
-        ...state,
-        inputs: {
-          ...state.inputs,
-          [action.name]: action.value
-        }
-      };
-    case 'CREATE_USER' :
-      return {
-        inputs: initialState.inputs,
-        users: state.users.concat(action.user)
-      };
-    case 'TOGGLE_USER' :
-      return {
-        ...state,
-        users: state.users.map(user =>
-            user.id === action.id ? {...user, active: !user.active} : user
-        )
-      };
-    case 'REMOVE_USER' :
-      return {
-        ...state,
-        users: state.users.filter(user => user.id !== action.id)
-      };
-    default:
-      return state;
-  }
-}
+`;
 
 function App() {
-  const [state, dispatch] = useReducer(reducer, initialState);
-  const nextId = useRef(4);
-  const { users } = state;
-  const {username, email} = state.inputs;
-
-  const onChange = useCallback(e => {
-    const {name, value} = e.target;
-    dispatch({
-      type: 'CHANGE_INPUT',
-      name,
-      value
-    });
-  }, []);
-
-  const onCreate = useCallback(() => {
-    dispatch({
-      type: 'CREATE_USER',
-      user: {
-        id: nextId.current,
-        username,
-        email
-      }
-    });
-    nextId.current += 1;
-  }, [username, email]);
-
-  const onToggle = useCallback(id => {
-    dispatch({
-      type: 'TOGGLE_USER',
-      id
-    });
-  }, []);
-
-  const onRemove = useCallback(id => {
-    dispatch({
-      type: 'REMOVE_USER',
-      id
-    });
-  }, []);
-
-  const count = useMemo(() => countActiveUsers(users), [users]);
-
   return (
       <>
-        <CreateUser username={username} email={email} onChange={onChange} onCreate={onCreate}/>
-        <UserList users={users} onToggle={onToggle} onRemove={onRemove}/>
-        <div>활성 사용자 수 : {count}</div>
+        <GlobalStyle/>
+        <TodoTemplate>
+          <TodoHead/>
+          <TodoList/>
+          <TodoCreate/>
+        </TodoTemplate>
       </>
   );
 }
